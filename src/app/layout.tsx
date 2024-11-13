a2ecf8af8e9b06c81bbcc318b01358f58ef9e829
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-11-13 20:53:47 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import UnderConstructionBanner from "../components/UnderConstructionBanner"
-import Header from "../components/header/Header";
+import Header from "../components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
