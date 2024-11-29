2b74c6e0328c995b07d2b217b1d100d444aabf40
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-11-29 18:52:32 +0530

    .

--- src/app/projects/[porjectId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProjectDetails({ params } : { params: { projectId: string }}) {
-    return (
-    <div>
-        <h1>DETAILS ABOUT PROJECT {params.projectId}</h1>
-    </div>
-    )
-}
